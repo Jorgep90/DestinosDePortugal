23c8a3742206e24db25219a66d41b22701f22813
Jorge <Jorge@DESKTOP-ATP05MJ>
2017-05-05 13:52:36 +0100

    wrb site base

---
+++ DestinosDePortugal/DestinosDePortugal/Controllers/Home_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DestinosDePortugal.Controllers
+{
+    public class Home_Controller : Controller
+    {
+        // GET: Home_
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
